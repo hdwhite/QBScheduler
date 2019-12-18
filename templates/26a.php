d53b8670dd59eda61af81f6303c3837b5fc0dbf2
Harry White <hdwhite@vt.edu>
2019-12-18 02:12:42 -0500

    Added a few pagebreaks

--- templates/26a.php
+++ templates/26a.php
@@ -38,6 +38,11 @@ $bracketparams['numteams'] = 6;
 		<p class="finals1" style="display:none">Each team will play a round-robin within their new brackets. After the final game, the top team in each of the two top brackets will play in a one-game final match, while the second, third, and fourth-place teams in these brackets will play their counterparts in the other top bracket to determine 3rd, 5th, 7th, and 9th place.</p>
 		<p class="finals2" style="display:none">Each team will play a round-robin within their new brackets. After the final game, there will be a four-team single-elimination bracket to determine the top places. The top team in each of the top two brackets will play the second-place team in the other bracket. The winners of those matches will play in the finals, while the losers will play for 3rd place. Meanwhile, the third and fourth-place teams in these brackets will play their counterparts in the other top bracket to determine 5th, 7th, and 9th place.</p>
 	</div>
+	<div class="pagebreak">
+		<div class="name">
+			<h2 class="tourneyname">&nbsp;</h2>
+		</div>
+	</div>
 	<div class="phase">
 		<div class="phaseheader">
 			<h2>Playoff Rounds</h2>
