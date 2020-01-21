fb122cf60bf906e2d54673b3b8bde1c89ebfc68d
Harry White <hdwhite@vt.edu>
2020-01-21 14:03:40 -0500

    Updated site header

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 		<li><a href="http://hdwhite.org/qb/stats">Database Search</a></li>
 		<li><a class='currentLink' href="http://hdwhite.org/qb/schedules">Schedule Generator</a></li>
 		<li><a href="https://hsquizbowl.org/forums/viewtopic.php?f=123&t=23356">Forum Thread</a></li>
-		<li><a href="http://hsquizbowl.org/db">HSQB Resource Database</a></li>
+		<li><a href="http://hsquizbowl.org/db">HSQB Database</a></li>
+		<li><a href="https://patreon.com/hdwhite">Patreon</a></li>
 	</ul>
 </div>
